a70371a6b2fd19b65105b788613c8b7131a492f4
Nel <nel.renaudin@gmail.com>
2022-10-10 16:27:07 +0200

    Add Task class to create task objects

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('working fine');
+class Task {
+  constructor(title, desc, dueDate) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.isDone = false;
+  }
+
+  switchIsDone() {
+    this.isDone = !this.isDone;
+  }
+}
